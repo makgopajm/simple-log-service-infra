8226004d4d4d60d39aaef43b952cd127b0a44a15
makgopajm <makgopajm@gmail.com>
2025-05-15 16:21:48 +0200

    Update Infra

--- iam.tf
+++ iam.tf
@@ -54,6 +54,8 @@ resource "aws_s3_bucket_policy" "s3_bucket_policy" {
             ]
         }
     )
+
+    depends_on = [ aws_s3_bucket_public_access_block.simple_log_service_s3_public_bloc ]
   
 }
 

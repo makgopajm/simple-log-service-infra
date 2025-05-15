2c631aa171d9bbe8fa96557136d230ed5d5e015f
makgopajm <makgopajm@gmail.com>
2025-05-15 16:52:29 +0200

    Update Infra

--- lambda.tf
+++ lambda.tf
@@ -27,8 +27,7 @@
 module "simple_log_service_write_lambda" {
     source = "./modules/lambda_function"
     function_name = "${var.product_name}-writer-lambda"
-    image_uri = module.simple_log_service_ecr_write_image.ecr_repo_uri
-    runtime = var.python_version
+    image_uri = "${module.simple_log_service_ecr_write_image.ecr_repo_uri}:latest"
     role_arn = aws_iam_role.lambda_write_role.arn
     
     environment_variables = {
@@ -39,8 +38,7 @@ module "simple_log_service_write_lambda" {
 module "simple_log_service_read_lambda" {
     source = "./modules/lambda_function"
     function_name = "${var.product_name}-reader-lambda"
-    image_uri = module.simple_log_service_ecr_read_image.ecr_repo_uri
-    runtime = var.python_version
+    image_uri = "${module.simple_log_service_ecr_read_image.ecr_repo_uri}:latest"
     role_arn = aws_iam_role.lambda_write_role.arn
     
     environment_variables = {
